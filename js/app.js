ad46ca796c321d602ba8817810124aee9fc0fade
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-07 21:31:22 +0100

    write controller and services

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module("photoBrowser", [])
+    .service("imageService", function ($http){
+        return {
+            getImages: function() {
+                return $http.get('/photos.json', { responseType: "json" });
+            }
+        }
+    })
+	.controller("photoList", function($scope, imageService) {
+        imageService.getImages().success(function (result) {
+            $scope.images = result;
+        })
+	});
